8f4cb250a84ef63d1546a8c1ac05821157c88d1e
Raheem <raheemideadev@gmail.com>
2021-08-28 22:58:30 +0500

    functions

--- app/js/index.js
+++ app/js/index.js
@@ -6,20 +6,5 @@ import $ from 'jquery';
 import { log, logTitle } from 'logger';
 
 /* code */
-logTitle("Arithmetic Operators");
+logTitle("Functions");
 
-/*
-     + - / % *
-*/
-
-var addition = 2 + 2;
-var subtraction = 3 - 19;
-var division = 10 / 5;
-var multipication = 2 * 30;
-var remainder = 9 % 5;
-
-log("2 + 2 = " + addition);
-log("3 - 19 = " + subtraction);
-log("10 / 5  = " + division);
-log("2 * 30 = " + multipication);
-log("9 % 5 = " + remainder);
