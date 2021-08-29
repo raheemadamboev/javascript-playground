62cffbb8877319218a0f918cc2354e34def7b915
Raheem <raheemideadev@gmail.com>
2021-08-29 14:01:03 +0500

    class

--- app/js/index.js
+++ app/js/index.js
@@ -6,12 +6,35 @@ import $ from 'jquery';
 import { log, logTitle } from 'logger';
 
 /* code */
-logTitle("Function Default Parameters");
+logTitle("Classes");
 
-const calculatePay = (salary, bonus = {
-    teamBonus: 0,
-    employeeBonus: 0,
-}) => {
-    return salary + bonus.teamBonus + bonus.employeeBonus;
-};
-log(calculatePay(5000, {employeeBonus: 20, teamBonus: 10}));
+class Animal {
+
+    constructor(name, age) {
+        log(`${name} was created!`)
+        this.name = name;
+        this.age = age;
+    }
+
+    eat() {
+        log(`${this.name} is eating!`);
+    }
+
+    sleep() {
+        log(`${this.name} is sleeping!`);
+    }
+
+    wakeUp() {
+        log(`${this.name} is waking up!`);
+    }
+
+    logAge() {
+        log(`${this.name} is ${this.age}`);
+    }
+}
+
+const bobby = new Animal("Bobby", 25);
+bobby.eat();
+bobby.sleep();
+bobby.wakeUp();
+bobby.logAge();
